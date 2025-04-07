fd52171bcfe40a0e351fedf65e028094af0115bc
nadim ahmmed <nadimahmmed2309@gmail.com>
2025-04-07 13:28:11 +0600

    navber

---
+++ fontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import Navbar from './components/Navbar'
+import { ThemeProvider } from "@/components/theme-provider"
+
+
+const App = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Navbar/>
+    </ThemeProvider>
+  )
+}
+
+export default App
